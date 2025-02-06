69e748fec39c9c108766504c386489a5bec0c518
ParkJeongBin01 <areoin@naver.com>
2025-02-06 10:47:24 +0900

    Update 특수문자 출력하기.java

--- 프로그래머스/0/181948. 특수문자 출력하기/특수문자 출력하기.java
+++ 프로그래머스/0/181948. 특수문자 출력하기/특수문자 출력하기.java
@@ -2,6 +2,6 @@ import java.util.Scanner;
 
 public class Solution {
     public static void main(String[] args) {    
-        System.out.print("!@#$%^&*(\\'\"<>?:;");
+        System.out.print("!@#$%^&*(\\'\"<>?:;"); // 이스케이프 문자(\)를 사용하여 따옴표를 올바르게 표현
     }
 }
