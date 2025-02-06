2513a10379dfb9340005780bd85f33902b2a71eb
ParkJeongBin01 <areoin@naver.com>
2025-02-06 09:52:25 +0900

    [level 0] Title: 제곱수 판별하기, Time: 3.04 ms, Memory: 89.3 MB -BaekjoonHub

--- 프로그래머스/0/120909. 제곱수 판별하기/README.md
+++ 프로그래머스/0/120909. 제곱수 판별하기/README.md
@@ -1,10 +1,10 @@
 # [level 0] 제곱수 판별하기 - 120909 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120909?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120909?language=java#) 
 
 ### 성능 요약
 
-메모리: 78.6 MB, 시간: 2.59 ms
+메모리: 89.3 MB, 시간: 3.04 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 02월 06일 09:49:49
+2025년 02월 06일 09:52:21
 
 ### 문제 설명
 
--- 프로그래머스/0/120909. 제곱수 판별하기/제곱수 판별하기.java
+++ 프로그래머스/0/120909. 제곱수 판별하기/제곱수 판별하기.java
@@ -1,10 +1,21 @@
 class Solution {
     public int solution(int n) {
+        int answer = 0;
         for(int i = 1; i <= n; i++){
-           if(i * i == n){
-               return 1;
-           }
+            if(i * i == n){
+                answer = 1;
+                break;
+            } else{
+                answer = 2;
+            }
         }
-        return 2;
+        return answer;
+        
+        // for(int i = 1; i <= n; i++){
+        //    if(i * i == n){
+        //        return 1;
+        //    }
+        // }
+        // return 2;
     }
 }
