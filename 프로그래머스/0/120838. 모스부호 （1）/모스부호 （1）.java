be4431ba25c170ae67b96aa80e7999dd9835870e
ParkJeongBin01 <areoin@naver.com>
2025-02-09 09:43:54 +0900

    Update 모스부호 （1）.java

--- 프로그래머스/0/120838. 모스부호 （1）/모스부호 （1）.java
+++ 프로그래머스/0/120838. 모스부호 （1）/모스부호 （1）.java
@@ -9,18 +9,17 @@ class Solution {
         // 3. letter를 공백 기준으로 나누어 각 문자에 대해 처리.
         String[] letters = letter.split(" ");
         for(String code : letters){
-            // 4. 인덱스를 이용하여 문자에 대해 처리
+            // 4. 각 모스부호의 인덱스를 찾고 문자를 추가.
             for(int i = 0; i <= morse.length; i++){
                 if(morse[i].equals(code)){
-                    builder.append((char) ('a' + i));
-                    break;
+                    builder.append((char) ('a' + i)); // 모스부호에 해당하는 문자를 추가.
+                    // 만약 i가 0이라면: 'a' + 0 → 97 → 'a', i가 1이라면: 'a' + 1 → 98 → 'b', i가 2라면: 'a' + 2 → 99 → 'c'
+                    break; // 일치하는 경우 루프 종료. 
                 }
             }
         }
-            
-       // 6. 최종 결과 문자열을 리턴.
-        return builder.toString();     
         
-
+       // 5. 최종 결과 문자열을 리턴.
+        return builder.toString();     
     }
 }
