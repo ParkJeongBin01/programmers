f8eec43e75806f4d9b755ea6f1ec12b684161a6a
ParkJeongBin01 <areoin@naver.com>
2025-02-12 09:50:10 +0900

    [level 0] Title: 삼각형의 완성조건 (1), Time: 1.17 ms, Memory: 73.6 MB -BaekjoonHub

--- 프로그래머스/0/120889. 삼각형의 완성조건 （1）/삼각형의 완성조건 （1）.java
+++ 프로그래머스/0/120889. 삼각형의 완성조건 （1）/삼각형의 완성조건 （1）.java
@@ -1,21 +1,26 @@
+import java.util.*;
 class Solution {
     public int solution(int[] sides) {
         // 1. 최대값을 담을 변수를 생성합니다.
-        int max = sides[0];
+//         int max = sides[0];
         
-        // 2. 배열에서 최대값을 찾습니다.
-        for(int i = 0; i < sides.length; i++){
-            max = Math.max(max, sides[i]);
-        }
+//         // 2. 배열에서 최대값을 찾습니다.
+//         for(int i = 0; i < sides.length; i++){
+//             max = Math.max(max, sides[i]);
+//         }
 
-        // 3. 나머지 값들의 합과 최대값을 비교하여 결과를 리턴합니다.
-        int sumOfOthers = sides[0] + sides[1] + sides[2] - max;
+//         // 3. 나머지 값들의 합과 최대값을 비교하여 결과를 리턴합니다.
+//         int sumOfOthers = sides[0] + sides[1] + sides[2] - max;
 
-        // 나머지 값들의 합이 최대값보다 크면 1, 작으면 2를 리턴합니다.
-        if (sumOfOthers > max) {
-            return 1; // 삼각형을 만들 수 있음
-        } else {
-            return 2; // 삼각형을 만들 수 없음
-        }
+//         // 나머지 값들의 합이 최대값보다 크면 1, 작으면 2를 리턴합니다.
+//         if (sumOfOthers > max) {
+//             return 1; // 삼각형을 만들 수 있음
+//         } else {
+//             return 2; // 삼각형을 만들 수 없음
+//         }
+//        ----------------------------------------------------------
+        int answer = 0;
+        Arrays.sort(sides); //오름차순으로 정렬
+        return sides[2] >= sides[0] + sides[1] ? 2 : 1;
     }
 }
