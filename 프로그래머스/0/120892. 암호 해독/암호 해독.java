e291e31a52d12d401a5de87b6dd1a52c64f2f1fa
ParkJeongBin01 <areoin@naver.com>
2025-02-21 18:56:40 +0900

    [level 0] Title: 암호 해독, Time: 1.91 ms, Memory: 75.4 MB -BaekjoonHub

--- 프로그래머스/0/120892. 암호 해독/암호 해독.java
+++ 프로그래머스/0/120892. 암호 해독/암호 해독.java
@@ -1,18 +1,31 @@
 class Solution {
     public String solution(String cipher, int code) {
-        String answer = "";
+//         String answer = "";
         
-        // 공백 포함 배열로 바꾼다.
-        String[] string = cipher.split("");
+//         // 입력 문자열을 문자 배열로 변환
+//         String[] string = cipher.split("");
         
-        // string length만큼 반복.
-        for(int i = 1; i < string.length+1; i++){
-            //code의 배수와 같으면 answer에 추가.
-            if(i % code == 0){
-                answer += string[i-1];
+//         // 각 문자를 순회하며 인덱스가 code의 배수인 경우 결과에 추가
+//         for(int i = 1; i < string.length; i++){
+//             // 현재 인덱스가 code의 배수인지 확인
+//             if((i + 1) % code == 0){
+//                 answer += string[i]; // 해당 문자를 결과에 추가
+//             }
+//         }
+//         //결과 반환
+//         return answer;
+//------------------------------------------------------------------------
+        StringBuilder answer = new StringBuilder();
+        
+         // 입력 문자열을 문자 배열로 변환
+         String[] string = cipher.split("");
+        
+        for(int i = 0; i < string.length; i++){
+            if((i + 1) % code == 0){
+                answer.append(string[i]); // 해당 문자를 결과에 추가
             }
         }
         
-        return answer;
+        return answer.toString();
     }
 }
