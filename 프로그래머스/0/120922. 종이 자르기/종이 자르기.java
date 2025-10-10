10027b91a3a8d7211991ca41307004a1e4fb8fb8
ParkJeongBin01 <areoin@naver.com>
2025-10-10 13:58:55 +0900

    [level 0] Title: 종이 자르기, Time: 0.01 ms, Memory: 80.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/120922. 종이 자르기/종이 자르기.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int solution(int M, int N) {
+        
+        
+        int x = M - 1;
+        int y = (N - 1) * M;
+        int answer = x+y;
+        return answer;
+    }
+}
