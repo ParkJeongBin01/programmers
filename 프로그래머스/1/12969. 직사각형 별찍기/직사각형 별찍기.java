7b566d94b9f065c3a3b6260e25be7ba3fc24f5c6
ParkJeongBin01 <areoin@naver.com>
2025-12-09 09:44:01 +0900

    [level 1] Title: 직사각형 별찍기, Time: 222.30 ms, Memory: 75.8 MB -BaekjoonHub

---
+++ 프로그래머스/1/12969. 직사각형 별찍기/직사각형 별찍기.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+class Solution {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        
+            for(int j = 0; j < b; j++){
+                System.out.println("*".repeat(a));
+            }
+        
+        
+    }
+}
