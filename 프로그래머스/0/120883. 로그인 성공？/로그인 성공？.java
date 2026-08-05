6d88740128816904ec4857dba521daf8d2ed50bb
ParkJeongBin01 <areoin@naver.com>
2026-08-05 13:22:00 +0900

    [level 0] Title: 로그인 성공?, Time: 0.02 ms, Memory: 84.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/120883. 로그인 성공？/로그인 성공？.java
@@ -0,0 +1,15 @@
+class Solution {
+    public String solution(String[] id_pw, String[][] db) {
+        
+        for(int i = 0; i < db.length; i++){
+            if(id_pw[0].equals(db[i][0])){ // 아이디 일치
+                if(id_pw[1].equals(db[i][1])){ // 비밀번호 일치
+                    return "login";
+                } else {
+                    return "wrong pw";
+                }
+            } 
+        }
+        return "fail";
+    }
+}
